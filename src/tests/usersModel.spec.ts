7e1001b46111bf97d771cfa6ce9f6ee4d3c9a3c3
omerenma <godwin2341@gmail.com>
2023-02-01 19:19:14 +0100

    test

--- src/tests/usersModel.spec.ts
+++ src/tests/usersModel.spec.ts
@@ -4,26 +4,24 @@ import { UserModel } from '../models/users'
 const store = new UserModel()
 const id: string = '1'
 const deleteId: number = 1
+const mockedToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoxMTcsImZpcnN0bmFtZSI6Ik5ldmVyIGV4cGlyZXMiLCJsYXN0bmFtZSI6Ik5ldmVyIGV4cGlyZXMgbGFzdG5hbWUiLCJwYXNzd29yZCI6IiQyYiQxMCRaZFhrMzVNRDhFSVNXTU5QTml4T3EuNjIyb1ZYOXpsdWl5RHdKQUQ2Zmc3YjJUcW83NHdjTyJ9LCJpYXQiOjE2NzUxNzc0MTYsImV4cCI6MzE3MjE5NjE5ODE2fQ.PLkLL4KQnC3boA-3SUDGtVqNZimayZKSMFyIPEFAoXM'
+
 describe('User Models', () => {
     it('should have an index method', async () => {
         const result = await store.index()
         expect(result).toBeDefined()
     })
-    it('should have a create method with id, firstname, lastname, password parameters', async () => {
-       const result = await store.create({firstname:'kingsley', lastname:'omerenma', password:'omerenma1'})
+    it('should have a create method with firstname, lastname, password parameters', async () => {
+       const result = await store.create({firstname:'kingsley', lastname:'omerenma', password:'omerenma1', token:mockedToken})
        expect(result).toBeDefined()
     })
     it('should have a show method with id parameter', async () => {
-        const result = await store.show(id)
+        const result = await store.show(1)
         expect(result).toBeDefined()
     })
      it('should have an edit method with id, firstname, lastname parameters', async () => {
         const result = await store.edit({id:1, firstname:'kingsley', lastname:'omerenma'})
         expect(result).toBeDefined()
      })
-    //  it('should have a delete method with id parameter', async () => {
-    //     const result = await store.deleteUser(deleteId)
-    //     expect(result).toBeDefined()
-    //  })
-    
+ 
 })
